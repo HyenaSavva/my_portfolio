cfe6f7ea66c440c0ff8e8c809ad86136a1d8469a
Ghenadie Savva <re4meister@gmail.com>
2026-01-28 17:16:32 +0200

    fix(build): cleared errors

--- src/app/router/layouts/__root.tsx
+++ src/app/router/layouts/__root.tsx
@@ -1,12 +1,12 @@
-import { useState, type FC } from "react";
+import { type FC } from "react";
 import { Outlet } from "@tanstack/react-router";
-import { Sidebar } from "@/widgets";
+// import { Sidebar } from "@/widgets";
 import { TooltipProvider } from "@/shared/ui/tooltip";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
 import { Greeting } from "@/shared/ui";
 
 export const RootLayout: FC = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <TooltipProvider>
